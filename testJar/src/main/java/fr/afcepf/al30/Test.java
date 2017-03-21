284c074a603dcabf6002ed059679b5518f533aa8
Ronan <ronan.lachater@gmail.com>
2017-03-21 14:33:36 +0100

    change git ignore

---
+++ testJar/src/main/java/fr/afcepf/al30/Test.java
@@ -0,0 +1,28 @@
+package fr.afcepf.al30;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Classe de test
+ * @author ronan
+ *
+ */
+public final class Test {
+	
+	/**
+	 * default constructor
+	 */
+	private Test() {
+		// private constructor
+	}
+	
+	private static Logger logger = Logger.getLogger(Test.class);
+	/**
+	 * Application entry point
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world...");
+	}
+
+}
